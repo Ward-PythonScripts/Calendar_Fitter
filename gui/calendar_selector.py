38ee391e565a9742e276d66b9b8c9d7d4cfc8d91
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-07-31 18:55:49 +0200

    we have grid with labels

--- gui/calendar_selector.py
+++ gui/calendar_selector.py
@@ -19,22 +19,55 @@ class Calendar_selector():
         return w
 
     def create_window_content(self):
-        intervals = []
         temp_time = 0
-        for day in range(0,7):
+        # first the interval labels will be generated
+        for x in range(math.floor((24 * 60) / self.amount_of_minutes_in_between)):
+            # then create the day label itself
+            temp_time += self.amount_of_minutes_in_between
+            MyIntervalLabel(begin_time=temp_time - self.amount_of_minutes_in_between, end_time=temp_time,
+                            master=self.main_window, row_pos=x + 1)
+        for day in range(0, 7):
             # first create the day header
-
+            MyDayLabel(master=self.main_window, day=day)
             for x in range(math.floor((24 * 60) / self.amount_of_minutes_in_between)):
-                #then create the day label itself
-                intervals.append(temp_time + self.amount_of_minutes_in_between)
+                # then create the day label itself
                 temp_time += self.amount_of_minutes_in_between
-                MyBox(day,start_time=temp_time-self.amount_of_minutes_in_between,end_time=temp_time,
-                      master=self.main_window,row_pos=x,col_pos=day)
+                MyBox(day, start_time=temp_time - self.amount_of_minutes_in_between, end_time=temp_time,
+                      master=self.main_window, row_pos=x + 1, col_pos=day + 1)
 
+
+def minutes_to_hour_string(minutes: int) -> str:
+    hours = math.floor(minutes / 60)
+    minutes = minutes - hours * 60
+    return pad_time(str(hours)) + ":" + pad_time(str(minutes))
+
+def pad_time(original_time:str) -> str:
+    if len(original_time) != 2:
+        return "0" + original_time
+    else:
+        return original_time
+
+def begin_and_end_to_string(begin_time: int, end_time: int) -> str:
+    s1 = minutes_to_hour_string(begin_time)
+    s2 = minutes_to_hour_string(end_time)
+    return s1 + " - " + s2
+
+
+class MyIntervalLabel:
+    def __init__(self, begin_time: int, end_time: int, master, row_pos):
+        label = tkinter.Label(text=(begin_and_end_to_string(begin_time, end_time)), master=master, justify="center")
+        label.grid(row=row_pos, column=0)
+
+
+class MyDayLabel:
+    def __init__(self, day: int, master):
+        days = ["Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun"]
+        label = tkinter.Label(text=days[day], master=master, justify="center")
+        label.grid(row=0, column=day+1)
 
 
 class MyBox:
-    def __init__(self, day: int, start_time: int, end_time: int, master,row_pos:int,col_pos:int):
+    def __init__(self, day: int, start_time: int, end_time: int, master, row_pos: int, col_pos: int):
         self.day = day
         self.start_time = start_time
         self.end_time = end_time
@@ -44,8 +77,9 @@ class MyBox:
         self.label = self.create_Label()
 
     def create_Label(self):
-        label = tkinter.Label(self.master,fg="green", height="2", width="4",text="hey")
-        label.grid(row=self.row_pos,column=self.col_pos)
+        label = tkinter.Label(self.master, fg="green", height="2", width="4", text="      ",borderwidth=5,relief="ridge",
+                              background="white")
+        label.grid(row=self.row_pos, column=self.col_pos)
         return label
 
     def button_clicked(self):

9ce3902cf95dffc3605897cb26ab91f07e5b6f02
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-08-04 15:43:39 +0200

    we kunnen tegoei selecteren en deselecteren
    we can properly track the start and end label
    
    can drag select

--- gui/calendar_selector.py
+++ gui/calendar_selector.py
@@ -1,14 +1,18 @@
 import tkinter
 from tkinter import *
 import math
+import pyautogui
+import time
 
 
 class Calendar_selector:
     def __init__(self, amount_of_minutes_in_between: int):
         self.amount_of_minutes_in_between = amount_of_minutes_in_between
         self.master = Tk()
+        self.master.title("Use the left mouse button to select and use the right mouse button to deselect")
         self.main_window = self.get_window()
         self.create_window_content()
+        self.add_person_selector()
         self.main_window.pack()
         mainloop()
 
@@ -18,7 +22,11 @@ class Calendar_selector:
         w = Canvas(self.master, width=canvas_width, height=canvas_height, background='gray75')
         return w
 
+    def add_person_selector(self):
+        pass
+
     def create_window_content(self):
+        global day_labels
         temp_time = 0
         # first the interval labels will be generated
         for x in range(math.floor((24 * 60) / self.amount_of_minutes_in_between)):
@@ -27,13 +35,16 @@ class Calendar_selector:
             MyIntervalLabel(begin_time=temp_time - self.amount_of_minutes_in_between, end_time=temp_time,
                             master=self.main_window, row_pos=x + 1)
         for day in range(0, 7):
+            row_of_labels = []
             # first create the day header
             MyDayLabel(master=self.main_window, day=day)
             for x in range(math.floor((24 * 60) / self.amount_of_minutes_in_between)):
                 # then create the day label itself
                 temp_time += self.amount_of_minutes_in_between
-                MyBox(day, start_time=temp_time - self.amount_of_minutes_in_between, end_time=temp_time,
-                      master=self.main_window, row_pos=x + 1, col_pos=day + 1)
+                row_of_labels.append(
+                    MyBox(day, start_time=temp_time - self.amount_of_minutes_in_between, end_time=temp_time,
+                          master=self.main_window, row_pos=x + 1, col_pos=day + 1))
+            day_labels.append(row_of_labels)
 
 
 def minutes_to_hour_string(minutes: int) -> str:
@@ -77,20 +88,120 @@ class MyBox:
         self.row_pos = row_pos
         self.col_pos = col_pos
         self.label = self.create_Label()
+        self.selected: bool = False
 
     def create_Label(self):
-        label = tkinter.Label(self.master, fg="green", height="2", width="4", text="      ", borderwidth=5,
+        label = tkinter.Label(self.master, fg="green", height="1", width="4", text="      ", borderwidth=5,
                               relief="ridge",
                               background="white")
         label.grid(row=self.row_pos, column=self.col_pos)
-        label.bind("<ButtonRelease-1>", lambda event,first_clicked_label=self: mouseReleasedOnLabel(event,
-                                                                            first_clicked_label=first_clicked_label))
+        label.bind("<ButtonRelease-1>",
+                   lambda event, first_clicked_label=self: leftMouseReleasedOnLabel(event,first_clicked_label))
+        label.bind("<ButtonRelease-3>",
+                   lambda event,label_clicked=self: rightMouseReleasedOnLabel(event,label_clicked))
         return label
 
+    def label_selected(self, targeted: bool):
+        if self.selected != targeted:
+            # something changed
+            self.change_color(targeted)
+            self.selected = targeted
 
-def mouseReleasedOnLabel(event: Event, first_clicked_label: MyBox):
+    def change_color(self, targeted: bool):
+        if targeted:
+            self.label.configure(background="green")
+        else:
+            self.label.configure(background="white")
+
+
+def leftMouseReleasedOnLabel(event: Event, label: MyBox):
+    mouseReleasedOnLabel(event=event, first_clicked_label=label, selected=True)
+
+
+def rightMouseReleasedOnLabel(event: Event, label: MyBox):
+    mouseReleasedOnLabel(event, label, False)
+
+
+def mouseReleasedOnLabel(event: Event, first_clicked_label: MyBox, selected: bool):
+    global mouse_event_handler
     # sadly we will have to find the label itself
-    # since the ButtonRelease-1 is already filled in when the mouse is pressed
-    print("Was pressed on row ", first_clicked_label.day)
-    #press the mouse again, and use that event to grab the second label.
+    # since the ButtonRelease-1 is already filled in when the mouse is pressed, and doesn't return the label on which
+    # was released
+    was_first = mouse_event_handler.add_event(first_clicked_label,selected)
+    # press the mouse again, and use that event to grab the second label. But we need to make sure we click the right
+    # mouse button of course
+    if was_first:
+        # if selected = true, left mouse. else right mouse was clicked
+        if selected:
+            left_click_again_to_get_release()
+        else:
+            right_click_again_to_get_release()
 
+
+
+def make_ascending(a1: int, b1: int) -> tuple[int, int]:
+    if a1 > b1:
+        # swap around
+        c1 = a1
+        a1 = b1
+        b1 = c1
+    return a1, b1
+
+
+def complete_selection(start_label: MyBox, end_label: MyBox,selected:bool):
+    # get the enclosed labels in between these two and trigger them as well, indexes in the MyBox object are starting
+    # at 1
+    x1 = start_label.row_pos - 1
+    x2 = end_label.row_pos - 1
+    y1 = start_label.col_pos - 1
+    y2 = end_label.col_pos - 1
+    # first we need to check if we are trying to go from small to big, otherwise won't work
+    x1, x2 = make_ascending(x1, x2)
+    y1, y2 = make_ascending(y1, y2)
+    for y in range(len(day_labels)):
+        for x in range(len(day_labels[y])):
+            # if we are in between x1,x2 and in between y1,y2. The selection is to have effect
+            if y1 <= y <= y2 and x1 <= x <= x2:
+                day_labels[y][x].label_selected(selected)
+
+
+def left_click_again_to_get_release():
+    pyautogui.mouseDown()
+    # sleep as a safeguard to make sure the click gets registered
+    time.sleep(0.004)
+    pyautogui.mouseUp()
+
+def right_click_again_to_get_release():
+    pyautogui.mouseDown(button="right")
+    # sleep as a safeguard to make sure the click gets registered
+    time.sleep(0.004)
+    pyautogui.mouseUp(button="right")
+
+class Mouse_Event_Handler:
+    def __init__(self):
+        self.first: bool = True
+        self.First_Label: MyBox = None
+        self.Second_Label: MyBox = None
+        self.selected:bool = False
+
+    def add_event(self, label: MyBox,selected:bool) -> bool:
+        self.selected = selected
+        if self.first:
+            self.first = False
+            self.First_Label = label
+            return True
+        else:
+            self.Second_Label = label
+            self.mouse_event_complete()
+            return False
+
+    def mouse_event_complete(self):
+        complete_selection(self.First_Label, self.Second_Label,self.selected)
+        self.First_Label = None
+        self.Second_Label = None
+        self.first = True
+
+
+###Globals
+mouse_event_handler = Mouse_Event_Handler()
+day_labels: list[list[MyBox]] = []
